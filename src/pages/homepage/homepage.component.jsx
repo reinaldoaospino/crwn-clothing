a5cf1d85fc9937849b79b430d8f90e6475b293e2
Reinaldo Ospino <reinaldo.aospino@gmail.com>
2020-05-24 13:09:55 -0300

    estilos a la pagina

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import Directory from '../../componets/directory/directory.component';
+
+import './homepage.sytles.scss';
+
+const HomePage = ()=>(
+    <div className='homepage'>
+        <Directory/>
+    </div>
+)
+
+export default HomePage;

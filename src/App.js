c64a94449e068ffa3b08025c621910ee8ba0b361
Reinaldo Ospino <reinaldo.aospino@gmail.com>
2020-05-28 19:55:25 -0300

    logIn with email and password

--- src/App.js
+++ src/App.js
@@ -32,9 +32,6 @@ class App extends React.Component {
               id: snapShot.id,
               ...snapShot.data()
             }
-          },()=>{
-            
-        console.log(this.state);
           });
         });
 

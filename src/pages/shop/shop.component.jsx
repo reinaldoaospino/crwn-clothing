c4f14f92dfcb9305ab183eb6fad9cff8f1f26c94
Reinaldo Ospino <reinaldo.aospino@gmail.com>
2020-06-05 14:10:57 -0300

    se agregar item cart reducer

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -6,7 +6,7 @@ import SHOP_DATA from './shop.data.js';
 
 class ShopPage extends React.Component{
     constructor(props){
-        super();
+        super(props);
 
         this.state={
             collections: SHOP_DATA
